27acdf7d64a61cf88760f7f76f788cafcb434012
linyicheng1 <hust_linyicheng@qq.com>
2024-07-20 13:20:10 +0800

    add trt export

--- utils/export.py
+++ utils/export.py
@@ -1,12 +1,12 @@
 import torch
 import openvino as ov
 import torch
-
+import tensorrt as trt
 
 def export_model(model, name):
     device = torch.device("cpu")
     model.to(device)
-    dummy_input = torch.randn(1, 3, 512, 512, requires_grad=True).to(device)
+    dummy_input = torch.randn(1, 3, 400, 800, requires_grad=True).to(device)
     torch.onnx.export(model,
                       dummy_input,
                       name+".onnx",
@@ -17,6 +17,34 @@ def export_model(model, name):
                       output_names=['score', 'descriptor'])
     ov_model = ov.convert_model(name+".onnx", example_input=dummy_input)
     ov.save_model(ov_model, name+'.xml')
+
+    # export to tensorRT
+    TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
+    builder = trt.Builder(TRT_LOGGER)
+    network = builder.create_network(1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
+    parser = trt.OnnxParser(network, TRT_LOGGER)
+
+    with open(name+".onnx", 'rb') as model_file:
+        if not parser.parse(model_file.read()):
+            for error in range(parser.num_errors):
+                print(parser.get_error(error))
+            raise Exception("Failed to parse the ONNX file")
+
+    config = builder.create_builder_config()
+    config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, 1 << 30)  # 1GB
+    serialized_engine = builder.build_serialized_network(network, config)
+
+    runtime = trt.Runtime(TRT_LOGGER)
+    engine = runtime.deserialize_cuda_engine(serialized_engine)
+
+    trt_engine_path = name + ".trt"
+    with open(trt_engine_path, "wb") as f:
+        f.write(engine.serialize())
+
+
+
+
+
 
 
 
